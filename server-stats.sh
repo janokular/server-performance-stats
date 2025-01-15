2c2802c2899f54ca40763aaa2b99204fae3f8703
Jan Okularczyk <janekokularczyk@gmail.com>
2025-01-15 18:29:44 +0100

    staged changes

--- server-stats.sh
+++ server-stats.sh
@@ -3,12 +3,26 @@
 # This script analysis basic server performance
 
 # Total CPU usage
+echo 'Total CPU usage:'
+# top
+echo
 
 # Total memory usage (Free vs Used including percentage)
+echo 'Total memory usage:'
+# free
+echo
 
 # Total disk usage (Free vs Used including percentage)
+echo 'Total disk usage:'
+# df
+echo
 
 # Top 5 processes by CPU usage
+echo 'Top 5 processes by CPU usage:'
+# ps
+echo
 
 # Top 5 processes by memory usage
-
+echo 'Top 5 processes by memory usage:'
+# ps
+echo
