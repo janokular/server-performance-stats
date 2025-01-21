dace801185b97ba7956254c17d881b654e444e5a
janokular <janekokularczyk@gmail.com>
2025-01-21 16:48:02 +0100

    staged changes

--- server-stats.sh
+++ server-stats.sh
@@ -2,25 +2,32 @@
 #
 # This script analysis basic server performance
 
+# Clear terminal window
+clear
+
 # Total CPU usage
-top -bn1 | grep "%Cpu(s):" | cut -d ',' -f 4 | awk '{print "Total CPU usage: " 100-$1 "%"}'
-grep "cpu[^0-9]" /proc/stat | awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}' | awk '{print "Total CPU usage: " 100-$1 "%"}'
+# Rounded to .01
+echo "Total CPU usage:"
+grep "cpu[^0-9]" /proc/stat | awk '{ print int((100 - ($5 * 100) / ($2 + $3 + $4 + $5 + $6 + $7 + $8 + $9 + $10)) * 100 + 0.5) / 100 "%" }'
 echo
 
 # Total memory usage (Free vs Used including percentage)
-free | grep "Mem:" | awk '{print "Total memory usage: " ($3/$2)*100 "%"}'
+# Rounded to .01
+echo "Total memory usage:"
+free | grep "Mem:" | awk '{ print int(($3 / $2) * 10000 + 0.5) / 100 "%" }'
 echo
 
 # Total disk usage (Free vs Used including percentage)
-df --total | grep "total" | awk '{print "Total disk usage: " $5}'
+echo "Total disk usage:"
+df --total | grep "total" | awk '{ print $5 }'
 echo
 
 # Top 5 processes by CPU usage
 echo 'Top 5 processes by CPU usage:'
-ps axu --sort %cpu | tail -n 5 | tac
+ps axu --sort %cpu | tail -n 5 | tac | awk '{ print $1 " " $2 " " $3 " " $11 }' | column -t
 echo
 
 # Top 5 processes by memory usage
 echo 'Top 5 processes by memory usage:'
-ps axu --sort %mem | tail -n 5 | tac
+ps axu --sort %mem | tail -n 5 | tac | awk '{ print $1 " " $2 " " $4 " " $11 }' | column -t
 echo
